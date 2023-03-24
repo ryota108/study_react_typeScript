46a7ef47d3cae65ab6f09df2a4944529f692170c
Ryota <aya.ryo.718@outlook.jp>
2023-03-24 23:42:24 +0900

    基本的な型の理解

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,26 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  let bool: boolean = true;
   let num: number = 0;
-  type User = {
-    id: number;
-    name: string;
-    tags: string[];
-  };
+  let str: string = "";
+  let arr1: Array<number> = [0];
+  let arr2: number[] = [0];
 
-  const user: User = {
-    id: 1,
-    name: "Yamada",
-    tags: [""],
-  };
+  let tuple : [number,string] = [0,"A"]
+
+  let any1:any = false;
+
+  let funcA = ():void =>{
+    console.log("ok")
+  }
+
+  let null1:null = null;
+
+  let undefined1:undefined = undefined;
+
+  let obj1 :{name:string} = {name:"ok"}
+
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/practice.tsx
@@ -0,0 +1,7 @@
+export const Practice1 = () => {
+  const calcTotalFee = (num: number) => {
+    const total: number = num * 1.1;
+    console.log(total);
+  };
+  return <></>;
+};
